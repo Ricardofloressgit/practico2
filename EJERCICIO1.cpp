386c086c1656b7eb8dcdb1dc7dadc6d96032def9
Ricardofloressgit <155786348+Ricardofloressgit@users.noreply.github.com>
2024-01-29 23:24:14 -0400

    Add files via upload

---
+++ EJERCICIO1.cpp
@@ -0,0 +1,118 @@
+/* el problema se debe de mostrar en orden el cual tiene que empezar del menu despues de escoger la opcion 1 se debe
+de mostrar el Submenu.
+EL PROGRAMA SE TIENE QUE REALIZAR DETRAR PARA DELANTE PORQUE LAS FUNCIONES TIENEN QUE ESTAR ECHAS ANTES QUE EL Menu
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+// Variables globales
+double num1, num2;
+string cadena1, cadena2;//nose porque en string no me muestra el color amarillo INVESTIGAR
+char opcion, subopcion;
+/*nota para se debe realizar primeramente el subproceso para poder llamar a la funcion
+Submenu esta va incluida en la primera opcion del Menu
+*/
+void Suma()
+{
+	cout<<"la suma de los dos numeros es "<<num1+num2<<endl;
+}
+void division()
+{
+	cout << "Ingrese el dividendo: ";
+	cin >> num1;
+	cout << "Ingrese el divisor: ";
+	cin >> num2;
+	if (num2 != 0) {
+		cout << "La división es: " << num1 / num2 << endl;
+	} else {
+		cout << "Error: División por cero." << endl;
+	}
+	
+}
+void CONCATENAR()
+{
+	cout<<"ingrese la primera cadena ";
+	cin>>cadena1;
+	cout<<"ingrese la segunda cadena ";
+	cin>>cadena2;
+	cout<<"el resultado de la concatenacion de las dos cadenas es : "<<cadena1+cadena2<<endl;
+	
+	
+}
+void Ingresar_datos()
+{
+	cout<<"Ingresa el primer número : ";
+	cin>>num1;
+	cout<<"Ingresa el segundo número : ";
+	cin>>num2;
+	Suma();
+}
+void Submenu()//se va a mostrar el submenu
+{
+	do
+	{
+		cout<<"   "<< " SUBMENU OP NUMEROS "<<endl<<endl; 
+		cout<<"1: Suma de 2 números. "<<endl<<endl;
+		cout<<"2: División de 2 números."<<endl<<endl;
+		cout<<"(m/M)Volver al Menu principal."<<endl<<endl;
+		cin>>subopcion;
+		switch(subopcion)
+		{
+		case '1':
+			cout<<"  "<<" SUMA "<<endl<<endl;
+			Ingresar_datos();
+			break;
+		case '2':
+			cout<<"  "<<" DIVISION "<<endl<<endl;
+			division();
+			break;
+			
+		case 'm':
+		case 'M':
+			return;
+			break;
+		default:
+			cout<<"opcion invalida";
+		};//fin switch
+	}while(subopcion != 'm'&&subopcion!='M');
+}
+
+void Menu()//funcion en las que se va a mostrar el menu
+{
+	do
+	{
+	cout<<"   "<< " MENU PRINCIPAL "<<endl<<endl; 
+	cout<<"1: Operaciones con Números"<<endl<<endl;
+	cout<<"2: Concatenar 2 cadenas."<<endl<<endl;
+	cout<<"(s/S): Salir."<<endl<<endl;
+	cin>>opcion;
+	switch(opcion)
+	{
+	case '1':
+		Submenu();
+	break;
+	case '2':
+		CONCATENAR();
+		
+	break;
+	
+	case 's':
+	case 'S':
+	break;
+	default:
+	cout<<"opcion invalida";
+	};//fin switch
+	}while(opcion != 's'&&opcion !='S');
+}
+	
+
+
+int main(){
+	
+	Menu();	
+
+
+	return 0;
+}
+	
---
+++ EJERCICIO2.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdlib> // Necesario para la función rand()
+#include <ctime> // Necesario para la función time()//para que los numeros aleatorios no se repitan
+#include <cmath>// Necesario para squr.
+using namespace std;
+void generar_Numero_al_azar(int &numero) 
+{
+	numero = rand() % 501;//se utiliza el 501 para que el 500 este entres los numeros aleatorios
+	cout << "Número al azar es : " << numero << endl;
+}//fin de funcion azar
+void Generar_numero_aleatorio_real(double &azar)
+{
+	azar = rand() % 501/100.0;//lo investigado decia que se tiene que hacer con 1000.0 pero solo generaba decimales es decir 0.98 siempre anteponiendo un 0
+	cout << "Número real aleatorio: " << azar << endl;
+}
+bool esPrimo(int &numero) {
+	if (numero <= 1) return false;
+	for (int i = 2; i <= sqrt(numero); ++i) //sqrt calcula la raíz cuadrada de un número.
+	{
+		if (numero % i == 0) return false;
+	}
+	return true;
+}
+void Generar_N_numeros_primos(int &Nprimo)
+{
+	int cont = 0, numero = 2,num1;
+	
+	cout<<"ingrese la cantidad de numeros a generar : ";
+	cin>>num1;
+	while(cont < num1){
+		if(esPrimo(numero)){
+			cont++;
+			cout<<"\n"<<cont<<":"<<"el numero primo es :" <<numero <<endl;
+		}
+		numero++;
+	}		
+	cout<<endl<<endl;
+}
+void Menu(char &opcion)
+{
+	
+	int numero,Nprimo;
+	double azar;
+	
+	do
+	{
+		cout<<"   "<<" MENU "<<endl<<endl;
+		cout<<"1: Genere al azar numero entre 0-500 "<<endl<<endl;
+		cout<<"2: Genere un numero aleatorio real "<<endl<<endl;
+		cout<<"3: Genere N números primos "<<endl<<endl;
+		cout<<"(s/S )-- Salir"<<endl;
+		cin>>opcion;
+		switch(opcion)
+		{
+		case '1':
+			
+			generar_Numero_al_azar(numero);
+			cout<<endl<<endl;
+			break;
+		case '2':
+			Generar_numero_aleatorio_real(azar);
+			cout<<endl<<endl;
+			break;
+		case '3':
+			
+			Generar_N_numeros_primos(Nprimo);
+			cout<<endl<<endl;
+			break;
+		case 's':
+		case 'S':
+			return;
+			break;
+		default:
+			cout<<"opcion invalida";
+		};//fin switch
+	}while(opcion != 's'&&opcion!='S');
+	
+	
+}
+int main(int argc, char *argv[]) {
+	srand(time(0));// Inicializar la semilla para obtener números realmente aleatorios
+	char opcion;
+	
+	Menu(opcion);
+	
+	return 0;
+}
+
